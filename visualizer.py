70fa3ff95955d17adc74ed84eef93606b1e5c03b
Luis González <luisglez02@gmail.com>
2020-03-29 14:34:40 +0200

    Added image re-centering

--- visualizer.py
+++ visualizer.py
@@ -38,7 +38,7 @@ class Visualizer:
 	
 	"""
 	
-	def __init__(self, exponent = mpf(2), max_iters = 100, threshold = mpf(2)):
+	def __init__(self, exponent = mpf(2), max_iters = 100, threshold = mpf(2), resolution = (800,800)):
 		"""Initialises the GUI and the canvas in which to draw
 		the fractal, apart from initializing the drawing limits
 		and resolution parameters
@@ -54,18 +54,45 @@ class Visualizer:
 		threshold: mpf
 			The maximum number at which a point will be considered to diverge (default: 2)
 		
+		resolution: tuple(int,int)
+			The width and height of the render space
+		
 		"""
 		
 		self.gui = Tk()
-		self.gui.geometry("800x800")
-		self.canvas = Canvas(self.gui, width = 800, height = 800)
+		self.gui.geometry("" + str(resolution[0]) + "x" + str(resolution[1]))
+		self.canvas = Canvas(self.gui, width = resolution[0], height = resolution[1])
 		self.canvas.pack()
 		self.generator = ImageGenerator(exponent, max_iters, threshold)
 		
 		self.topLeftPoint = mpc(-2,2)
 		self.bottomRightPoint = mpc(2,-2)
-		self.realRes = 800
-		self.imgRes = 800
+		self.realRes = resolution[0]
+		self.imgRes = resolution[1]
+		
+		def center_image_callback(event):
+			"""Callback for clicking somewhere inside the fractal image.
+			This will set the center to wherever the click was made
+			"""
+			
+			# Calculate the complex number corresponding to the point that was clicked
+			realRelativePos = mpf(event.x) / mpf(self.realRes)
+			imgRelativePos = mpf(event.y) / mpf(self.imgRes)
+			realOffset = realRelativePos * (self.bottomRightPoint.real - self.topLeftPoint.real)
+			imgOffset = imgRelativePos * -(self.bottomRightPoint.imag - self.topLeftPoint.imag)
+			realNewCenter = self.topLeftPoint.real + realOffset
+			imgNewCenter = self.topLeftPoint.imag - imgOffset
+			
+			# Calculate the current center and apply the (new - current) transform to the corners
+			realCurrentCenter = (self.bottomRightPoint.real + self.topLeftPoint.real) / 2
+			imgCurrentCenter = (self.bottomRightPoint.imag + self.topLeftPoint.imag) / 2
+			absoluteDisplacement = mpc(realNewCenter, imgNewCenter) - mpc(realCurrentCenter, imgCurrentCenter)
+			self.topLeftPoint = self.topLeftPoint + absoluteDisplacement
+			self.bottomRightPoint = self.bottomRightPoint + absoluteDisplacement
+			self.draw()
+		
+		self.canvas.bind("<Button-1>", center_image_callback)
+		
 		
 	def draw(self):
 		"""Draws the fractal in the canvas of the main window
@@ -75,12 +102,10 @@ class Visualizer:
 		None
 		
 		"""
-		#image = self.generator.generateImage(self,topLeftPoint,
-		#									 self.bottomRightPoint,
-		#									 self.realRes,
-		#									 self.imgRes)
-
-		image = Image.open("test.png")
+		image = self.generator.generateImage(self.topLeftPoint,
+											 self.bottomRightPoint,
+											 self.realRes,
+											 self.imgRes)
 		
 		image = ImageTk.PhotoImage(image)
 		
@@ -93,7 +118,7 @@ class Visualizer:
 
 if (__name__ == "__main__"):
 	
-	window = Visualizer()
+	window = Visualizer(max_iters = 5, resolution = (400,400))
 	window.draw()
 		
 		
